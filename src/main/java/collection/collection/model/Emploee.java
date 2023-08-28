a8950f9822f20f8ffb5c930d82be19b762a830e9
Олег <olegcelysev778@gmail.com>
2023-08-28 15:35:32 +0300

    коммит

---
+++ src/main/java/collection/collection/model/Emploee.java
@@ -0,0 +1,43 @@
+package collection.collection.model;
+
+import java.util.Objects;
+
+public class Emploee {
+        private final String firstName;
+        private final String lasttName;
+
+        public Emploee(String firstName, String lusttName) {
+            this.firstName = firstName;
+            this.lasttName = lusttName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLusttName() {
+            return lasttName;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Emploee emploee = (Emploee) o;
+            return Objects.equals(firstName, emploee.firstName) && Objects.equals(lasttName, emploee.lasttName);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(firstName, lasttName);
+        }
+
+        @Override
+        public String toString() {
+            return "Emploee{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lusttName='" + lasttName + '\'' +
+                    '}';
+        }
+    }
+
